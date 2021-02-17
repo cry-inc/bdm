a70a5128109b93f2f0bda46d0e79a45b06faef03
cry-inc <cry-inc@users.noreply.github.com>
2021-02-17 20:19:14 +0100

    Moved handlers for static assets into same file

--- pkg/bdm/server/handlerstatic.go
+++ pkg/bdm/server/handlerstatic.go
@@ -7,12 +7,22 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+//go:embed static/index.html
+var html []byte
+
+//go:embed static/favicon.ico
+var icon []byte
+
+func createHTMLHandler() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(html)
+	}
+}
+
 func createFaviconHandler() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		w.Header().Set("Content-Type", "image/x-icon")
 		w.Write(icon)
 	}
 }
-
-//go:embed static/favicon.ico
-var icon []byte
