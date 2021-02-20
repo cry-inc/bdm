37553782f9d2720ed30fdff60c44abeb427c8471
cry-inc <cry-inc@users.noreply.github.com>
2021-02-20 23:35:57 +0100

    Implemented basic limits structure and checking function

---
+++ pkg/bdm/limits.go
@@ -0,0 +1,41 @@
+package bdm
+
+import "fmt"
+
+// ManifestLimits represents constraint for packages.
+// The dfault value zero means unlimited.
+type ManifestLimits struct {
+	MaxFileSize   int64
+	MaxSize       int64
+	MaxFilesCount int
+	MaxPathLength int
+}
+
+// CheckManifestLimits can check if a manifest is within the given package limmits.
+// It will return nil if the manifest is within the limits, otherwise an error.
+func CheckManifestLimits(manifest *Manifest, limits *ManifestLimits) error {
+	if limits.MaxFilesCount > 0 && len(manifest.Files) > limits.MaxFilesCount {
+		return fmt.Errorf("Number of files is %d exceeds the limit of %d",
+			len(manifest.Files), limits.MaxFilesCount)
+	}
+
+	var overallSize int64 = 0
+	for _, file := range manifest.Files {
+		overallSize += file.Object.Size
+		if limits.MaxPathLength > 0 && len(file.Path) > limits.MaxPathLength {
+			return fmt.Errorf("Path length of %d exceeds the limit of %d",
+				len(file.Path), limits.MaxPathLength)
+		}
+		if limits.MaxFileSize > 0 && file.Object.Size > limits.MaxFileSize {
+			return fmt.Errorf("File size of %d exceeds the limit of %d",
+				file.Object.Size, limits.MaxFileSize)
+		}
+	}
+
+	if limits.MaxSize > 0 && overallSize > limits.MaxSize {
+		return fmt.Errorf("File size of %d exceeds the limit of %d",
+			overallSize, limits.MaxSize)
+	}
+
+	return nil
+}
