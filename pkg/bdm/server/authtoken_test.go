482ac82af9b479a158baa92eeaf1aa5fe6930869
cry-inc <cry-inc@users.noreply.github.com>
2021-05-05 21:51:59 +0200

    Implemented auth tokens (simplified JWTs) to be used for browser login mechanism

---
+++ pkg/bdm/server/authtoken_test.go
@@ -0,0 +1,37 @@
+package server
+
+import "testing"
+
+func TestJwtEndToEnd(t *testing.T) {
+	// Generate a JWT
+	token := CreateAuthToken("foo@bar.com")
+
+	// Parse & Validate JWT token and extract user
+	userId, err := ReadAuthToken(token)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if userId != "foo@bar.com" {
+		t.Fatal(userId)
+	}
+}
+
+func TestGetUserIdFromJwt(t *testing.T) {
+	// Invalid token layout without two dots
+	_, err := ReadAuthToken("abc")
+	if err == nil {
+		t.Fatal()
+	}
+
+	// Invalid base64 data in signature part
+	_, err = ReadAuthToken("a.a")
+	if err == nil {
+		t.Fatal()
+	}
+
+	// Invalid signature
+	_, err = ReadAuthToken("a.eyJmb28iOiAiYmFyIn0=")
+	if err == nil {
+		t.Fatal()
+	}
+}
