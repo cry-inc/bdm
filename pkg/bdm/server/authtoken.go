be5f391ed32867996b694fe6deb52f54aef0772c
cry-inc <cry-inc@users.noreply.github.com>
2021-05-11 19:46:35 +0200

    Reworked auth tokens for web user interface

--- pkg/bdm/server/authtoken.go
+++ pkg/bdm/server/authtoken.go
@@ -11,9 +11,15 @@ import (
 	"time"
 )
 
+const defaultExpiration time.Duration = 365 * 24 * time.Hour
+
 var secret = make([]byte, 512)
 
-const expiresAfter = "24h"
+type authToken struct {
+	UserId  string
+	Expires time.Time
+	Token   string
+}
 
 func init() {
 	n, err := rand.Read(secret)
@@ -22,61 +28,65 @@ func init() {
 	}
 }
 
-func CreateAuthToken(userId string) string {
-	expireDuration, err := time.ParseDuration(expiresAfter)
-	if err != nil {
-		panic(err)
-	}
-	currentTime := time.Now().Add(expireDuration)
-	expireStr := fmt.Sprintf("%d", currentTime.Unix())
-	signedData := expireStr + "." + base64.StdEncoding.EncodeToString([]byte(userId))
+func createAuthToken(userId string, expiration time.Duration) authToken {
+	expires := time.Now().Add(expiration)
+	expiresStr := fmt.Sprintf("%d", expires.Unix())
+	signedData := expiresStr + "." + base64.StdEncoding.EncodeToString([]byte(userId))
 	signer := hmac.New(sha512.New, secret)
 	n, err := signer.Write([]byte(signedData))
 	if n != len(signedData) || err != nil {
 		panic(err)
 	}
 	signature := signer.Sum(nil)
-	authToken := signedData + "." + base64.StdEncoding.EncodeToString(signature)
-	return authToken
+	token := signedData + "." + base64.StdEncoding.EncodeToString(signature)
+	return authToken{
+		UserId:  userId,
+		Expires: expires,
+		Token:   token,
+	}
 }
 
-func ReadAuthToken(token string) (string, error) {
+func readAuthToken(token string) (*authToken, error) {
 	parts := strings.Split(token, ".")
 	if len(parts) != 3 {
-		return "", fmt.Errorf("input string is not a valid auth token")
+		return nil, fmt.Errorf("input string is not a valid auth token")
 	}
 
 	signature, err := base64.StdEncoding.DecodeString(parts[2])
 	if err != nil {
-		return "", fmt.Errorf("failed to decode base64 signature")
+		return nil, fmt.Errorf("failed to decode base64 signature")
 	}
 
 	signedData := parts[0] + "." + parts[1]
 	signer := hmac.New(sha512.New, secret)
 	_, err = signer.Write([]byte(signedData))
 	if err != nil {
-		return "", fmt.Errorf("failed to write data for HMAC: %w", err)
+		return nil, fmt.Errorf("failed to write data for HMAC: %w", err)
 	}
 
 	expectedSignature := signer.Sum(nil)
 	if !hmac.Equal(expectedSignature, signature) {
-		return "", fmt.Errorf("detected invalid signature")
+		return nil, fmt.Errorf("detected invalid signature")
 	}
 
-	expirationTime, err := strconv.ParseInt(parts[0], 10, 64)
+	expiresUnix, err := strconv.ParseInt(parts[0], 10, 64)
 	if err != nil {
-		return "", fmt.Errorf("failed to parse expiration date: %w", err)
+		return nil, fmt.Errorf("failed to parse expiration date: %w", err)
 	}
 
-	currentTime := time.Now().Unix()
-	if currentTime > expirationTime {
-		return "", fmt.Errorf("auth token already expired")
+	currentUnix := time.Now().Unix()
+	if currentUnix > expiresUnix {
+		return nil, fmt.Errorf("auth token already expired")
 	}
 
 	userId, err := base64.StdEncoding.DecodeString(parts[1])
 	if err != nil {
-		return "", fmt.Errorf("failed to decode base64 payload")
+		return nil, fmt.Errorf("failed to decode base64 payload")
 	}
 
-	return string(userId), nil
+	return &authToken{
+		UserId:  string(userId),
+		Expires: time.Unix(expiresUnix, 0),
+		Token:   token,
+	}, nil
 }
