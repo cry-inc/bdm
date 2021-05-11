308bffcde88a41815a02eca72599102905c2ccc7
cry-inc <cry-inc@users.noreply.github.com>
2021-05-11 19:46:35 +0200

    Reworked auth tokens for web user interface

--- pkg/bdm/server/authtoken_test.go
+++ pkg/bdm/server/authtoken_test.go
@@ -1,36 +1,54 @@
 package server
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestJwtEndToEnd(t *testing.T) {
 	// Generate a JWT
-	token := CreateAuthToken("foo@bar.com")
+	token := createAuthToken("foo@bar.com", defaultExpiration)
+	if token.UserId != "foo@bar.com" {
+		t.Fatal(token)
+	}
+	if time.Now().After(token.Expires) {
+		t.Fatal(token)
+	}
+	if len(token.Token) == 0 {
+		t.Fatal(token)
+	}
 
 	// Parse & Validate JWT token and extract user
-	userId, err := ReadAuthToken(token)
+	readToken, err := readAuthToken(token.Token)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if userId != "foo@bar.com" {
-		t.Fatal(userId)
+	if readToken.Token != token.Token {
+		t.Fatal(readToken)
+	}
+	if readToken.UserId != "foo@bar.com" {
+		t.Fatal(readToken)
+	}
+	if readToken.Expires.Equal(token.Expires) {
+		t.Fatal(readToken)
 	}
 }
 
 func TestGetUserIdFromJwt(t *testing.T) {
 	// Invalid token layout without two dots
-	_, err := ReadAuthToken("abc")
+	_, err := readAuthToken("abc")
 	if err == nil {
 		t.Fatal()
 	}
 
 	// Invalid base64 data in signature part
-	_, err = ReadAuthToken("0.a.a")
+	_, err = readAuthToken("0.a.a")
 	if err == nil {
 		t.Fatal()
 	}
 
 	// Invalid signature
-	_, err = ReadAuthToken("0.a.eyJmb28iOiAiYmFyIn0=")
+	_, err = readAuthToken("0.a.eyJmb28iOiAiYmFyIn0=")
 	if err == nil {
 		t.Fatal()
 	}
