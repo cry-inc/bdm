8f3631fee857aa9d4013ff02665827fe9fbe2810
cry-inc <cry-inc@users.noreply.github.com>
2021-05-16 16:26:10 +0200

    Migrated store tests to new assert helpers

--- pkg/bdm/objects_test.go
+++ pkg/bdm/objects_test.go
@@ -2,6 +2,7 @@ package bdm
 
 import (
 	"io"
+	"reflect"
 	"testing"
 
 	"github.com/cry-inc/bdm/pkg/bdm/util"
@@ -30,7 +31,5 @@ func TestObjects(t *testing.T) {
 
 	read, err := ReadObjectsFromStream(reader)
 	util.AssertNoError(t, err)
-	util.Assert(t, len(read) == len(objects))
-	util.Assert(t, read[0] == objects[0])
-	util.Assert(t, read[1] == objects[1])
+	util.Assert(t, reflect.DeepEqual(read, objects))
 }
--- pkg/bdm/store/store_test.go
+++ pkg/bdm/store/store_test.go
@@ -16,75 +16,48 @@ const storeFolder = "./store"
 
 func TestStore(t *testing.T) {
 	defer os.RemoveAll(storeFolder)
-
 	store, err := New(storeFolder)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Get package names from empty store
 	names, err := store.GetNames()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(names) != 0 {
-		t.Fatal("Store should be empty")
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, len(names) == 0)
 
 	// Try package that does not exist
 	versions, err := store.GetVersions("doesnotexist")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(versions) != 0 {
-		t.Fatal("Non-existing package must return no versions")
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, len(versions) == 0)
 
 	// Create test object #1
 	objectData1 := []byte{1, 2, 3, 5, 5}
 	objectSize1 := len(objectData1)
 	objectHash1, err := util.HashStream(bytes.NewReader(objectData1))
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Ask for object that is not yet in the store
 	_, err = store.GetObject(objectHash1)
-	if err == nil {
-		t.Fatal("Object should not exist")
-	}
+	util.AssertError(t, err)
 
 	// Publish object #1 to store
 	buffer1 := bytes.NewReader(objectData1)
 	object1, err := store.AddObject(buffer1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if object1.Hash != objectHash1 || object1.Size != int64(objectSize1) {
-		t.Fatal()
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, object1.Hash == objectHash1)
+	util.Assert(t, object1.Size == int64(objectSize1))
 
 	// Check if object #2 was correctly added to the store
 	object1, err = store.GetObject(objectHash1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if object1.Hash != objectHash1 || object1.Size != int64(objectSize1) {
-		t.Fatal()
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, object1.Hash == objectHash1)
+	util.Assert(t, object1.Size == int64(objectSize1))
 
 	// Read object #1 from store and compare returned data
 	reader, err := store.ReadObject(objectHash1)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 	readData, err := ioutil.ReadAll(reader)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(readData, objectData1) {
-		t.Fatal("Data should not differ")
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, reflect.DeepEqual(readData, objectData1))
 
 	// Create valid unpublished manifest #1 with object #1
 	manifest1 := bdm.Manifest{
@@ -104,39 +77,25 @@ func TestStore(t *testing.T) {
 
 	// Publish manifest #1 and check assigned version number
 	err = store.PublishManifest(&manifest1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if manifest1.PackageVersion != 1 {
-		t.Fatal("Version should be changed to published version")
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, manifest1.PackageVersion == 1)
 
 	// Check if package list contains published manifest #1
 	names, err = store.GetNames()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(names) != 1 || names[0] != manifest1.PackageName {
-		t.Fatal()
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, len(names) == 1)
+	util.Assert(t, names[0] == manifest1.PackageName)
 
 	// Check if list of versions for manifest #1 is working
 	versions, err = store.GetVersions(manifest1.PackageName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(versions) != 1 || versions[0] != 1 {
-		t.Fatal()
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, len(versions) == 1)
+	util.Assert(t, versions[0] == 1)
 
 	// Read publish manifest and compare with original
 	readManifest, err := store.GetManifest(manifest1.PackageName, manifest1.PackageVersion)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(*readManifest, manifest1) {
-		t.Fatal()
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, reflect.DeepEqual(*readManifest, manifest1))
 
 	// Create manifest #2 as unpublished copy of manifest #1
 	manifest2 := manifest1
@@ -146,21 +105,15 @@ func TestStore(t *testing.T) {
 
 	// Try to publish copy of same content as new version, should fail
 	err = store.PublishManifest(&manifest2)
-	if err == nil {
-		t.Fatal()
-	}
+	util.AssertError(t, err)
 	var dupErrr DuplicatePackageError
-	if !errors.As(err, &dupErrr) {
-		t.Fatal(err)
-	}
+	util.Assert(t, errors.As(err, &dupErrr))
 
 	// Create object #2
 	objectData2 := []byte{6, 7, 8}
 	objectSize2 := len(objectData2)
 	objectHash2, err := util.HashStream(bytes.NewReader(objectData2))
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Create manifest #3 as copy of #1 with additional object #2
 	manifest3 := manifest1
@@ -179,49 +132,32 @@ func TestStore(t *testing.T) {
 	manifest3.Hash = bdm.HashManifest(&manifest3)
 
 	// Object "completeness" check should fail since object #2 ist not yet added
-	if AllObjectsExist(&manifest3, store) {
-		t.Fatal("Object 2 should not exist in store")
-	}
+	util.Assert(t, !AllObjectsExist(&manifest3, store))
 
 	// Add object #2 to store and check again
 	_, err = store.AddObject(bytes.NewBuffer(objectData2))
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !AllObjectsExist(&manifest3, store) {
-		t.Fatal()
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, AllObjectsExist(&manifest3, store))
 
 	// Now we try again to publish manifest #3 as new package version #2 and it should work
 	err = store.PublishManifest(&manifest3)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if manifest3.PackageVersion != 2 {
-		t.Fatal("Version should be changed to published version")
-	}
+	util.AssertNoError(t, err)
+	util.Assert(t, manifest3.PackageVersion == 2)
 
 	// Trigger store validation, which should succeed
 	stats, err := ValidateStore(store)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Check store validatsion stats
-	if stats["objects"] != 2 ||
-		stats["size"] != 8 ||
-		stats["packages"] != 2 {
-		t.Fatal(stats)
-	}
+	util.Assert(t, stats["objects"] == 2)
+	util.Assert(t, stats["size"] == 8)
+	util.Assert(t, stats["packages"] == 2)
 }
 
 func TestCacheStore(t *testing.T) {
 	defer os.RemoveAll(storeFolder)
-
 	store, err := New(storeFolder)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Fake a published manifest
 	manifest := bdm.Manifest{
@@ -243,13 +179,9 @@ func TestCacheStore(t *testing.T) {
 
 	// A manifest that is marked as published cannot be published again!
 	err = store.PublishManifest(&manifest)
-	if err == nil {
-		t.Fatal()
-	}
+	util.AssertError(t, err)
 
 	// Add valid published manifest directly to the store without publishing works
 	err = store.AddManifest(&manifest)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 }
