934244598e56c948fe2b36404c55bf39ffb6e0d3
cry-inc <cry-inc@users.noreply.github.com>
2021-05-16 15:41:48 +0200

    Unified existing helpers for handler testing

--- pkg/bdm/server/server_test.go
+++ pkg/bdm/server/server_test.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"net/http"
-	"net/url"
 	"strings"
 	"testing"
 )
@@ -29,54 +27,22 @@ func TestReplacePort(t *testing.T) {
 	}
 }
 
-type FakeResponse struct {
-	Status  int
-	Headers http.Header
-	Data    string
-}
-
-func (r *FakeResponse) Write(data []byte) (int, error) {
-	r.Data = r.Data + string(data)
-	return len(data), nil
-}
-
-func (r *FakeResponse) WriteHeader(statusCode int) {
-	r.Status = statusCode
-}
-
-func (r *FakeResponse) Header() http.Header {
-	return r.Headers
-}
-
 func TestCreateFallbackHandler(t *testing.T) {
 	handler := createFallbackHandler(2323)
 
-	url, _ := url.Parse("/path")
-	response1 := FakeResponse{
-		Headers: make(http.Header),
-	}
-	request1 := http.Request{
-		Host:   "myhostname.com",
-		Method: "GET",
-		URL:    url,
-	}
-
-	handler(&response1, &request1)
-	if response1.Headers["Location"][0] != "https://myhostname.com:2323/path" {
+	request1 := createMockedRequest("GET", "/path", nil, nil)
+	request1.Host = "myhostname.com"
+	response1 := createMockedResponse()
+	handler(response1, request1)
+	if response1.headers["Location"][0] != "https://myhostname.com:2323/path" {
 		t.Fatal(response1)
 	}
 
-	response2 := FakeResponse{
-		Headers: make(http.Header),
-	}
-	request2 := http.Request{
-		Host:   "myhostname.com",
-		Method: "POST",
-		URL:    url,
-	}
-
-	handler(&response2, &request2)
-	if response2.Status != 400 || !strings.Contains(response2.Data, "Use HTTPS") {
+	request2 := createMockedRequest("POST", "/path", nil, nil)
+	request2.Host = "myhostname.com"
+	response2 := createMockedResponse()
+	handler(response2, request2)
+	if response2.status != 400 || !strings.Contains(string(response2.data), "Use HTTPS") {
 		t.Fatal(response2)
 	}
 }
