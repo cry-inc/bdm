4ad8ef2302face70a107d978cef75acc7fdc7f67
cry-inc <cry-inc@users.noreply.github.com>
2021-05-16 16:33:06 +0200

    Ported client package tests to new assert helpers

--- pkg/bdm/client/check_test.go
+++ pkg/bdm/client/check_test.go
@@ -7,38 +7,29 @@ import (
 	"testing"
 
 	"github.com/cry-inc/bdm/pkg/bdm"
+	"github.com/cry-inc/bdm/pkg/bdm/util"
 )
 
 func TestCheckFiles(t *testing.T) {
 	const testFolder = "../../../test/example"
 
 	manifest, err := bdm.GenerateManifest("foo", testFolder)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Clean folder should pass check with clean enabled
 	err = CheckFiles(manifest, testFolder, true)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	junkFile := path.Join(testFolder, "bla")
 	err = ioutil.WriteFile(junkFile, []byte{123}, os.ModePerm)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 	defer os.Remove(junkFile)
 
 	// Dirty folder should pass check with clean disabled
 	err = CheckFiles(manifest, testFolder, false)
-	if err != nil {
-		t.Fatal(err)
-	}
+	util.AssertNoError(t, err)
 
 	// Dirty folder should fail check with clean enabled
 	err = CheckFiles(manifest, testFolder, true)
-	if err == nil {
-		t.Fatal()
-	}
+	util.AssertError(t, err)
 }
