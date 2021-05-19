a6f7bf8f1a3930ebec7dabd467eecd23e7a2e8dd
cry-inc <cry-inc@users.noreply.github.com>
2021-05-19 21:43:41 +0200

    Implemented packages Vue component

--- pkg/bdm/server/static/components/packages.js
+++ pkg/bdm/server/static/components/packages.js
@@ -1,3 +1,21 @@
 export default {
-	template: `<div>Packages</div>`
+	data() {
+		return {
+			packages: []
+		};
+	},
+	async created() {
+		const response = await fetch('manifests');
+		const packages = await response.json();
+		this.packages = packages;
+	},
+	template: `
+		<div>
+			<h1>Packages</h1>
+			<ul>
+				<li v-for="package in packages">
+					<router-link v-bind:to="'/' + package.Name">{{package.Name}}</router-link>
+				</li>
+			</ul>
+		</div>`
 }
