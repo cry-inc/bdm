72c442784c8932b03887865a8850718a3bcfec59
cry-inc <cry-inc@users.noreply.github.com>
2021-05-19 22:24:50 +0200

    Implemented Vue package component

--- pkg/bdm/server/static/app.js
+++ pkg/bdm/server/static/app.js
@@ -1,12 +1,32 @@
 import Packages from './components/packages.js'
 import Versions from './components/versions.js'
+import Package from './components/package.js'
 
 const router = VueRouter.createRouter({
 	history: VueRouter.createWebHashHistory(),
 	routes: [
 		{path: '/', component: Packages},
 		{path: '/:package', component: Versions, props: true},
+		{path: '/:package/:version', component: Package, props: true},
 	]
 });
 
-Vue.createApp({}).use(router).mount('#bdm');
+const app = Vue.createApp({});
+app.use(router);
+app.config.globalProperties.$filters = {
+	size(bytes) {
+		if (bytes < 1024) {
+			return bytes + " byte";
+		} else if (bytes < 1000 * 1000) {
+			return Math.round(bytes / 1000) + " kB";
+		} else if (bytes < 1000 * 1000 * 1000) {
+			return Math.round(bytes / (1000 * 1000)) + " MB";
+		} else {
+			return Math.round(bytes / (1000 * 1000 * 1000)) + " GB";
+		}
+	},
+	date(unixTime) {
+		return new Date(unixTime * 1000).toLocaleString();
+	}
+};
+app.mount('#bdm');
