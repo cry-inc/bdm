a5f93f3ab1a5698aa93873ce11247939fcdafd80
cry-inc <cry-inc@users.noreply.github.com>
2021-06-01 20:31:17 +0200

    Implemented login and logout in user menu UI component

--- pkg/bdm/server/static/components/user-menu.js
+++ pkg/bdm/server/static/components/user-menu.js
@@ -3,6 +3,9 @@ export default {
 		return {
 			user: null,
 			usersEnabled: false,
+			showLoginForm: false,
+			userId: '',
+			password: ''
 		};
 	},
 	async created() {
@@ -13,15 +16,53 @@ export default {
 			const response = await fetch('login');
 			this.usersEnabled = response.status !== 503;
 			this.user = response.ok ? await response.json() : null;
+		},
+		async toggleLogin() {
+			this.showLoginForm = !this.showLoginForm;
+		},
+		async login() {
+			const request = {
+				UserId: this.userId,
+				Password: this.password
+			};
+			const response = await fetch('/login', {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(request)
+			});
+			if (!response.ok) {
+				this.password = '';
+				alert('Login failed!');
+			} else {
+				this.userId = '';
+				this.password = '';
+				this.showLoginForm = false;
+				this.query();
+			}
+		},
+		async logout() {
+			await fetch('/login', {method: 'DELETE'});
+			this.query();
 		}
 	},
 	template: `
 		<div class="usermenu">
-			<div v-if="usersEnabled === false || !user">
+			<span class="guest" v-if="!usersEnabled || !user">
 				Guest
-			</div>
-			<div v-if="user">
+			</span>
+			<span v-if="user">
 				{{user.Id}}
+			</span>
+			<button v-if="usersEnabled && !user" @click="toggleLogin">
+				Login
+			</button>
+			<button v-if="user" @click="logout">
+				Logout
+			</button>
+			<div class="loginform" v-if="showLoginForm">
+				User ID: <input v-model="userId" placeholder="User ID"></input><br>
+				Password: <input v-model="password" type="password" placeholder="Password"></input><br>
+				<button @click="login">Login</button>
 			</div>
 		</div>`
 }
