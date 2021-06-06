b42163959aa954bde8a9447881f3d150ff738b47
cry-inc <cry-inc@users.noreply.github.com>
2021-06-06 19:57:19 +0200

    Added UI to change passwords

--- pkg/bdm/server/static/components/user.js
+++ pkg/bdm/server/static/components/user.js
@@ -3,13 +3,31 @@ export default {
 	data() {
 		return {
 			user: null,
-			loaded: false
+			loaded: false,
+			newPassword: ''
 		};
 	},
 	async created() {
 		const response = await fetch('users/' + this.userId);
 		this.user = response.ok ? await response.json() : null;
 		this.loaded = true;
+	},
+	methods: {
+		async changePassword() {
+			const request = {
+				Password: this.newPassword
+			};
+			const response = await fetch('/users/' + this.userId + '/password', {
+				method: 'PATCH',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(request)
+			});
+			if (!response.ok) {
+				alert('Failed to change password!');
+			} else {
+				this.newPassword = '';
+			}
+		}
 	},
 	template: `
 		<div>
@@ -22,6 +40,9 @@ export default {
 				Reader: {{user.Reader ? 'yes' : 'no'}}<br>
 				Writer: {{user.Writer ? 'yes' : 'no'}}<br>
 				Admin: {{user.Admin ? 'yes' : 'no'}}<br>
+				<h2>Change Password</h2>
+				New Password: <input v-model="newPassword" type="password" placeholder="New Password"/><br>
+				<button @click="changePassword">Change Password</button>
 				<tokens v-bind:userId="user.Id"></tokens>
 			</div>
 		</div>`
