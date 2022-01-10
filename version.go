34d5b585b588859a69bf388cc702b9f265929add
cry-inc <cry-inc@users.noreply.github.com>
2022-01-10 20:35:38 +0100

    Moved version and build info to separate file

---
+++ version.go
@@ -0,0 +1,10 @@
+package main
+
+// BDM version (optionally injected during build)
+var bdmVersion = "n/a"
+
+// git commit hash of build (optionally injected during build)
+var commitHash = "n/a"
+
+// Date string of this build (optionally injected during build)
+var buildDate = "n/a"
